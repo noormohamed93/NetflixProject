738d98e1730afcc62fc7931029cb864626ef730f
norhan mohamed <norhan.mohamed254@ci.menofia.edu.eg>
2022-09-04 05:53:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//QUESTION1
+const targetDiv =document.getElementById('answer1');
+const btn = document.getElementById('q1');
+btn.onclick = function (){
+    if(targetDiv.style.display !=="none"){
+        targetDiv.style.display = "none";
+    }else{
+        targetDiv.style.display = "block";
+    }
+}
+//QUESTION2
+const question2 =document.getElementById('answer2');
+const question2btn = document.getElementById('q2');
+question2btn.onclick = function (){
+    if(question2.style.display !=="none"){
+        question2.style.display = "none";
+    }else{
+        question2.style.display = "block";
+    }
+}
+//QUESTION3
+const question3 =document.getElementById('answer3');
+const question3btn = document.getElementById('q3');
+question3btn.onclick = function (){
+    if(question3.style.display !=="none"){
+        question3.style.display = "none";
+    }else{
+        question3.style.display = "block";
+    }
+}
+//QUESTION4
+const question4 =document.getElementById('answer4');
+const question4btn = document.getElementById('q4');
+question4btn.onclick = function (){
+    if(question4.style.display !=="none"){
+        question4.style.display = "none";
+    }else{
+        question4.style.display = "block";
+    }
+}
+//QUESTION5
+const question5 =document.getElementById('answer5');
+const question5btn = document.getElementById('q5');
+question5btn.onclick = function (){
+    if(question5.style.display !=="none"){
+        question5.style.display = "none";
+    }else{
+        question5.style.display = "block";
+    }
+}
+//QUESTION6
+const question6 =document.getElementById('answer6');
+const question6btn = document.getElementById('q6');
+question6btn.onclick = function (){
+    if(question6.style.display !=="none"){
+        question6.style.display = "none";
+    }else{
+        question6.style.display = "block";
+    }
+}
